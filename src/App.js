e2d5e32312cbb42d01fc0c7c9dd2fec591eea4a6
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2023-08-03 15:11:51 +0500

    authentication done with firebase

--- src/App.js
+++ src/App.js
@@ -1,33 +1,84 @@
-import {Box,Button,Container, HStack, Input, VStack} from '@chakra-ui/react'
-import Message from './Components/Message';
+import { app } from "./firebase";
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  HStack,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import Message from "./Components/Message";
+import {
+  onAuthStateChanged,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+
+const auth = getAuth(app);
+
+const loginHandler = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+};
+
+const logOutHandler = ()=>{
+  signOut(auth)
+}
 
 function App() {
+  const [user, setUser] = useState(false);
 
+  useEffect(() => {
+  const unSubscribe =   onAuthStateChanged(auth, (data) => {
+      setUser(data);
+    });
+
+    return ()=>{
+      unSubscribe()
+    }
+  });
 
   return (
-    <Box bg={'red.50'}>
-      <Container h={'100vh'} bg={'white'}>
-      <VStack h='full' padding={'4 0'} >
-        {/* VStack is a flex and has fles direction colum by default */}
-        <Button w='full' colorScheme={'pink'} >Logout⬆️</Button>
-        <VStack h='full' w={'full'} overflow={'auto'}>   
-        <Message text={'Sample Message'} user='other'/>
-        <Message text={'Sample Message'} user='me'/>
-        
-           </VStack>
+    <Box bg={"red.50"}>
+      {user ? (
+        <Container h={"100vh"} bg={"white"}>
+          <VStack h="full" padding={"4 0"}>
+            {/* VStack is a flex and has fles direction colum by default */}
+            <Button w="full" colorScheme={"pink"} onClick={logOutHandler}>
+              Logout⬆️
+            </Button>
+            <VStack h="full" w={"full"} overflow={"auto"}>
+              <Message text={"Sample Message"} user="other" />
+              <Message text={"Sample Message"} user="me" />
+            </VStack>
 
-        <form style={{ width:'100%'}}>
-          <HStack marginBottom={'4'}>
-          <Input placeholder='Enter a Message...' />
-          <Button colorScheme={'purple'} type='submit'>Send➡️</Button>
-          </HStack>
-        </form>
-
-  
-
-
-      </VStack>
-      </Container>
+            <form style={{ width: "100%" }}>
+              <HStack marginBottom={"4"}>
+                <Input placeholder="Enter a Message..." />
+                <Button colorScheme={"purple"} type="submit">
+                  Send➡️
+                </Button>
+              </HStack>
+            </form>
+          </VStack>
+        </Container>
+      ) : (
+        <VStack
+          bg="white"
+          color={"blackAlpha.500"}
+          h="100vh"
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Button colorScheme={"yellow"} onClick={loginHandler}>
+            Sign In With Google 🌐
+          </Button>
+        </VStack>
+      )}
     </Box>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCu8gbnK-5qyWvM8rdPs4RKqcal-5RrUnY",
+  authDomain: "chatapp-1b49b.firebaseapp.com",
+  projectId: "chatapp-1b49b",
+  storageBucket: "chatapp-1b49b.appspot.com",
+  messagingSenderId: "702132219106",
+  appId: "1:702132219106:web:e6616163d546611b1b42bb"
+};
+
+
+export const app = initializeApp(firebaseConfig);
